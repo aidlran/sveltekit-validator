5e38d5711d44f8af16dc3a7bc34731eee16e2f19
Aidan Loughran <86127892+aidlran@users.noreply.github.com>
2023-03-19 19:54:17 +0000

    Fix: remove wip decorator

--- dist/decorators/form-data.decorator.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function FormData(target: object, propertyKey: string | symbol, parameterIndex: number): void;
--- dist/decorators/form-data.decorator.js
+++
@@ -1,5 +0,0 @@
-export function FormData(target, propertyKey, parameterIndex) {
-    console.log(target);
-    console.log(propertyKey);
-    console.log(parameterIndex);
-}
